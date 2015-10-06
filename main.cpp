ab4bcc08661e33e11691bdc673ae3067753dbcb8
Roope Savolainen <roope.savolainen@gmail.com>
2015-10-06 23:10:54 +0300

    Lots of small modifications Disabled b&w to make MOG2 work, added debugging mode, added parameters to motion tracking and image simplification phases...

--- main.cpp
+++ main.cpp
@@ -3,28 +3,40 @@
 #include <thread>
 #include <mutex>
 #include <chrono>
+#include <string>
 
 #include "camera.hpp"
 #include "motiontrack.hpp"
 
 int main(int argc, char** argv)
 {
-    cv::VideoCapture cap = initCamera();
+	bool debugMode = false;
 
-    cv::namedWindow("Alkuperainen", cv::WINDOW_NORMAL);
-    cv::namedWindow("Suodatettu", cv::WINDOW_NORMAL);
+	//Not a good way to handle this, but will do for now since we probably won't have any more commmand line arguments
+	if (argc > 1 && std::string(argv[1]) == "--DEBUG") debugMode = true;
+	
+    cv::VideoCapture cap = initCamera();
+	
+	if (debugMode)
+	{
+		cv::namedWindow("Alkuperainen", cv::WINDOW_NORMAL);
+		cv::namedWindow("Suodatettu", cv::WINDOW_NORMAL);
+		cv::namedWindow("Tausta", cv::WINDOW_NORMAL);
+	}
 
     bool keepRunning = true;
     
     std::mutex frameMutex;
     cv::Mat _frame;
-   
-    cap.read(_frame);
+
+	cap.read(_frame);
+
     std::thread frameThread(frameUpdate, &keepRunning, &_frame, &frameMutex, &cap);
 
-	MotionTrack motion;
+	MotionTrack motion(8, 32, 0.05);
 
     cv::Mat frame;
+	int pressed;
     while(keepRunning)
     {
         frameMutex.lock();
@@ -32,11 +44,16 @@ int main(int argc, char** argv)
         frameMutex.unlock();
 	
 		motion.UpdateFrame(frame);
-        
-        cv::imshow("Alkuperainen", frame);
-        cv::imshow("Suodatettu", motion.getResult());
+		
+		if (debugMode)
+		{	
+			cv::imshow("Alkuperainen", frame);
+			cv::imshow("Suodatettu", motion.getResult());
+			cv::imshow("Tausta", motion.getBackground());
+		}
 
-        if (cv::waitKey(1) == 113) keepRunning = false;
+		pressed = cv::waitKey(30);
+        if ((char)pressed == 'q') keepRunning = false;
     }
 	
     frameThread.join();
