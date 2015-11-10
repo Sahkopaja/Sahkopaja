a58bede164119086d410f43bc7b4dcceed1b8035
Roope Savolainen <roope.savolainen@gmail.com>
2015-11-10 18:48:40 +0200

    Clustering

--- motiontrack.cpp
+++ motiontrack.cpp
@@ -1,7 +1,12 @@
 #include "motiontrack.hpp"
 #include <stdio.h>
+#include <map>
+#include <cmath>
 
+static const int MOMENT_MERGE_THRESH = 400;
 
+float distance(int x1, int x2, int y1, int y2, float area1, float area2);
+void handleGrouping(std::vector<std::vector<unsigned> > &neighbors, std::map<unsigned, bool> &handled, unsigned i);
 
 void MotionTrack::UpdateFrame(cv::Mat newFrame)
 {
@@ -23,6 +28,8 @@ void MotionTrack::UpdateFrame(cv::Mat newFrame)
 
 	cv::findContours( tmpFrame.clone(), contours, hierarchy, CV_RETR_CCOMP, CV_CHAIN_APPROX_SIMPLE );
 
+	moments = {};
+
 	if (hierarchy.size() > 0)
 	{
 		for (int i = 0; i >= 0; i = hierarchy[i][0])
@@ -32,11 +39,130 @@ void MotionTrack::UpdateFrame(cv::Mat newFrame)
 			if ( area < minArea || area > maxArea )
 			{
 				moments.push_back(moment);
-				cv::circle(tmpFrame, cv::Point(moment.m10/area, moment.m01/area), 20, cv::Scalar(255,255),2);
 			}
 		}
 	}
 
+	targets = MergeMoments(moments);
+
+	for (unsigned i = 0; i < targets.size(); i++)
+	{
+		cv::circle(tmpFrame, cv::Point(targets[i].getX(), targets[i].getY()), 20, cv::Scalar(255,255),2);
+	}
+
 	resultFrame = tmpFrame;
+}
+
+std::vector<Target> MotionTrack::MergeMoments(std::vector<cv::Moments> moments)
+{
+	unsigned x1, x2, y1, y2;
+	float area1, area2;
+
+	std::vector<std::vector<unsigned> > neighbors;
+	std::vector<std::vector<cv::Moments> > groups;
+
+	std::vector<Target> _targets = {};
+
+	for (unsigned i = 0; i < moments.size(); i++)
+	{
+		auto m = moments[i];
+		x1 = m.m10/m.m00;
+		y1 = m.m01/m.m00;
+		area1 = m.m00;
+		
+		std::vector<unsigned> v = {};
+		neighbors.push_back(v);
+		
+		for (unsigned j = 0; j <= moments.size(); j++)
+		{
+			if (i != j)
+			{
+				auto n = moments[j];
+				x2 = n.m10/n.m00;
+				y2 = n.m01/n.m00;
+				area2 = n.m00;
+				if (distance(x1, y1, x2, y2, area1, area2) < MOMENT_MERGE_THRESH)
+				{
+					neighbors[i].push_back(j);
+				}
+			}
+		}
+	}
+	
+	std::map<unsigned, int> to_handle = {};
+	for (unsigned i = 0; i < moments.size(); i++)
+	{
+		to_handle[i] = -1;
+	}
+
+	for (unsigned i = 0; i < moments.size(); i++)
+	{
+		if (to_handle.find(i) != to_handle.end())
+		{
+			int val = to_handle.find(i)->second;
+			if (val == -1)
+			{
+				std::vector<cv::Moments> v = {moments[i]};
+				val = groups.size();
+
+				groups.push_back(v);
+			}
+			else
+			{
+				groups[val].push_back(moments[i]);
+				for (unsigned j = 0; j <= neighbors[i].size(); i++)
+				{
+					if (to_handle.find(neighbors[i][j]) != to_handle.end())
+					{
+						to_handle[neighbors[i][j]] = val;
+					}
+				}
+			}
+			to_handle.erase(i);
+		}
+	}
+
+	for (unsigned i = 0; i < groups.size(); i++)
+	{
+		int min_x = 999999, max_x = 0, min_y = 999999, max_y = 0;
+		for (unsigned j = 0; j <= groups[i].size(); j++)
+		{
+			cv::Moments m = groups[i][j];
+			int _x = m.m10/m.m00;
+			int _y = m.m01/m.m00;
+			if (_x < min_x)
+			{min_x = _x;}
+			if (_x > max_x)
+			{max_x = _x;}
+			if (_y < min_y)
+			{min_y = _y;}
+			if (_y > max_y)
+			{max_y = _y;}
+		}
+		int x = (min_x + max_x);
+		int y = (min_y + max_y);
+		int w = max_x - min_x + MOMENT_MERGE_THRESH;
+		int h = max_y - min_y + MOMENT_MERGE_THRESH;
+		Target t(x, y, w, h);
+		_targets.push_back(t);
+	}
+
+	return _targets;
+}
+
+float distance(int x1, int y1, int x2, int y2, float area1, float area2)
+{
+	int _x, _y;
+	if (x1 < x2)
+	{_x = (x2 - sqrt(area2)) - (x1 + sqrt(area1));}
+	else
+	{_x = (x1 - sqrt(area1)) - (x2 + sqrt(area2));}
+
+	if (y1 < y2)
+	{_y = (y2 - sqrt(area2)) - (y1 + sqrt(area1));}
+	else
+	{_y = (y1 - sqrt(area1)) - (y2 + sqrt(area2));}
+
+	return sqrt(pow(_x, 0.5) + pow(_y, 0.5));
 }
 
--- motiontrack.hpp
+++ motiontrack.hpp
@@ -9,6 +9,8 @@ static const double THRESHOLD = 3.0;
 static const int MIN_AREA = 400;
 static const int MAX_AREA = 2000;
 
+struct Target;
+
 //MotionTrack class will wrap most motion tracking functionality, which will be called from main()
 class MotionTrack
 {
@@ -24,6 +26,7 @@ class MotionTrack
         std::vector<cv::Vec4i> hierarchy;
 
         std::vector<cv::Moments> moments;
+		std::vector<Target> targets;
 
 		double learningRate;
 		int blurStrength;
@@ -39,10 +42,27 @@ class MotionTrack
 			learningRate = _learningRate;
 		}
 
+		std::vector<Target> MergeMoments(std::vector<cv::Moments> moments);
+
 		void UpdateFrame(cv::Mat newFrame);
 
 		cv::Mat getResult() {return resultFrame.clone();}
 		cv::Mat getBackground() {return background.clone();}
+};
+
+//Simple struct to contain targets to shoot at
+struct Target
+{
+	private:
+		unsigned x, y, w, h;
+	
+	public:
+		Target(unsigned _x, unsigned _y, unsigned _w, unsigned _h) : x(_x), y(_y), w(_w), h(_h) {}
+
+		unsigned getX() {return x;}
+		unsigned getY() {return y;}
+		unsigned getW() {return w;}
+		unsigned getH() {return h;}
 };
 
 #endif
