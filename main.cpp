6581e71d652eea8b9e90ff0b192eb02ca270fa4b
Sahkopaja <sahkopajapi@gmail.com>
2015-12-29 15:59:07 +0200

    Update main.cpp
    Sanitized indent

--- main.cpp
+++ main.cpp
@@ -108,21 +108,21 @@ int main(int argc, char** argv)
 
 	cv::VideoCapture cap = initCamera(&preferences);
 
-    bool graphicMode = (preferences.readInt("graphic_mode", 1) != 0);
-    
-    if (debugMode && graphicMode)
-    {
-        //Setup video windows
-        cv::namedWindow("Video", cv::WINDOW_NORMAL);
-        cv::namedWindow("Suodatettu", cv::WINDOW_NORMAL);
-        cv::namedWindow("Tausta", cv::WINDOW_NORMAL);
-    }
+	bool graphicMode = (preferences.readInt("graphic_mode", 1) != 0);
+
+	if (debugMode && graphicMode)
+	{
+		//Setup video windows
+		cv::namedWindow("Video", cv::WINDOW_NORMAL);
+		cv::namedWindow("Suodatettu", cv::WINDOW_NORMAL);
+		cv::namedWindow("Tausta", cv::WINDOW_NORMAL);
+	}
 
 	//Set up the thread to read frames from camera feed
 	cv::Mat _frame;
 	cap.read(_frame);
-    std::mutex frameMutex;
-    std::thread frameThread(frameUpdate, &keepRunning, &_frame, &frameMutex, &cap);
+	std::mutex frameMutex;
+	std::thread frameThread(frameUpdate, &keepRunning, &_frame, &frameMutex, &cap);
 
 	//Set up the thread handling hardware interaction
 	double targetX = -1, targetY = -1;
@@ -140,18 +140,18 @@ int main(int argc, char** argv)
 	TargetValidator validator = TargetValidator(sampleSize, minimumSampleRatio, &preferences);
 	std::pair<double, double> targetLocation;
 
-    std::chrono::duration<int, std::milli> resetDuration(preferences.readInt("reset_duration", 5000));
-    std::chrono::system_clock::time_point resetBeginTime = std::chrono::system_clock::now();
+	std::chrono::duration<int, std::milli> resetDuration(preferences.readInt("reset_duration", 5000));
+	std::chrono::system_clock::time_point resetBeginTime = std::chrono::system_clock::now();
 
 	ProgramState state = ProgramState::RESET;
 
 	//The main loop of the program
-    while(keepRunning)
-    {
+	while(keepRunning)
+	{
 		//Read a frame from camera thread
-        frameMutex.lock();
-        frame = _frame;
-        frameMutex.unlock();
+        	frameMutex.lock();
+        	frame = _frame;
+        	frameMutex.unlock();
 
 		//Do motion detection calculations
 		motion.UpdateFrame(frame);
@@ -168,10 +168,10 @@ int main(int argc, char** argv)
 			if (std::chrono::system_clock::now() - resetBeginTime > resetDuration)
 			{
 				state = ProgramState::ACQUIRE;
-                if (debugMode)
-                {
-                    std::cout << "Entering Acquire state\n";
-                }
+                		if (debugMode)
+				{
+					std::cout << "Entering Acquire state\n";
+				}
 			}
 		}
 		//Move to elimination phase if target is found
@@ -181,10 +181,10 @@ int main(int argc, char** argv)
 			{
 				startShooting = true;
 				state = ProgramState::ELIMINATE;
-                if (debugMode)
-                {
-                    std::cout << "Entering Eliminate state\n";
-                }
+				if (debugMode)
+				{
+					std::cout << "Entering Eliminate state\n";
+				}
 			}
 		}
 		//Aim at the target if found
@@ -198,12 +198,12 @@ int main(int argc, char** argv)
 			if (shootingDone)
 			{
 				state = ProgramState::RESET;
-                resetBeginTime = std::chrono::system_clock::now();
+				resetBeginTime = std::chrono::system_clock::now();
 				shootingDone = false;
-                if (debugMode)
-                {
-                    std::cout << "Entering Reset state\n";
-                }
+				if (debugMode)
+				{
+					std::cout << "Entering Reset state\n";
+				}
 			}
 
 			hwMutex.unlock();
@@ -219,13 +219,13 @@ int main(int argc, char** argv)
 
 		//For quitting the program press q (only works in debug mode, use ctrl-c in other cases)
 		pressed = cv::waitKey(1);
-        if ((char)pressed == 'q') keepRunning = false;
-    }
+		if ((char)pressed == 'q') keepRunning = false;
+	}
 
 	std::cout << "\nExiting...\n";
 
 	//Wait for other threads to close
-    frameThread.join();
+	frameThread.join();
 	hwThread.join();
 
 	return 0;
